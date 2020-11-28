9af37011ee317c083f2ddde8499be1fc8987da99
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-11-28 21:59:06 +0530

    feat: add 10 python solutions

---
+++ hackerrank/python/loops.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    for num in range(n):
+        print(num**2)
---
+++ hackerrank/python/print_function.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    for ltr in range(1,n+1):
+        print(ltr,end="")
